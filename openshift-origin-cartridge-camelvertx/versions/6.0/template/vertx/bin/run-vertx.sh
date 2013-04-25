2a0aee3eb7f5f5b13c8331de1a2a0ac261087990
William DeCoste <bdecoste@gmail.com>
2013-04-25 15:31:51 -0700

    update vertx

--- openshift-origin-cartridge-camelvertx/versions/6.0/template/vertx/bin/run-vertx.sh
+++ openshift-origin-cartridge-camelvertx/versions/6.0/template/vertx/bin/run-vertx.sh
@@ -5,4 +5,4 @@ export VERTX_HOME=$(dirname $0)/../vert.x-1.3.1.final
 echo "PWD: $PWD"
 echo "VERTX_HOME: $VERTX_HOME"
 
-$VERTX_HOME/bin/vertx run  ./server.js  -cluster -cluster-port 25502 -cluster-host 127.0.0.1
+$VERTX_HOME/bin/vertx run  ./server.js  -cluster -cluster-port 25502 -cluster-host ${env.OPENSHIFT_CARTVERTX_IP}
